818cf2255e87b0252eb2e13c616444b25b30de15
Michael Arnaldi <michael@creditscript.com>
2019-04-06 14:40:54 +0100

    close server after test

--- lib/middleware.test.ts
+++ lib/middleware.test.ts
@@ -3,28 +3,32 @@ import express = require("express");
 import { wireToExpress, defRQ, T, numberT } from ".";
 
 describe("Test the root path", () => {
+  const port = 3000;
+  const app = express();
+
+  const barR = defRQ({
+    page: "foo",
+    pattern: "/test",
+    query: T.required({
+      n: numberT
+    })
+  });
+
+  wireToExpress(_ => ({
+    render: (_, res) => {
+      res.status(200).send("");
+    }
+  }))([barR], app);
+
+  const server = app.listen(port, () => {});
+
   test("It should response the GET method", () => {
-    const port = 3000;
-    const app = express();
-
-    const barR = defRQ({
-      page: "foo",
-      pattern: "/test",
-      query: T.required({
-        n: numberT
-      })
-    });
-
-    wireToExpress(_ => ({
-      render: (_, res) => {
-        res.status(200).send("");
-      }
-    }))([barR], app);
-
-    app.listen(port, () => {});
-
     return request(app)
       .get("/test?n=10")
       .expect(200);
+  });
+
+  test("Stop server", () => {
+    server.close()
   });
 });
